8b7938d703355d07fb74084d6161d542f12326d7
seg4lt <12027886+seg4lt@users.noreply.github.com>
2024-04-06 12:13:32 -0500

    Basic cout of acks

--- src/cmd_processor/process_wait_cmd.rs
+++ src/cmd_processor/process_wait_cmd.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::TcpStream, sync::Arc};
+use std::{io::Write, net::TcpStream, sync::Arc, time::Instant};
 
 use anyhow::Context;
 use tracing::debug;
@@ -16,7 +16,51 @@ pub fn process_wait_cmd(
         debug!("No replicas to wait for. Returning 0 as acks received.");
         return Ok(DataType::Integer(0));
     }
-    return Ok(DataType::Integer(replicas_len as u64));
+    let get_ack_command = DataType::Array(vec![
+        DataType::BulkString("REPLCONF".into()),
+        DataType::BulkString("GETACK".into()),
+        DataType::BulkString("*".into()),
+    ]);
+    let get_ack_command_len = get_ack_command.as_bytes().len();
+    let start_time = Instant::now();
+
+    let mut ack_received = 0;
+    let mut processed_replicas = 0;
+    'main_loop: loop {
+        if ack_received >= nacks_wanted {
+            break 'main_loop;
+        }
+        if start_time.elapsed().as_millis() > timeout_ms as u128 {
+            break 'main_loop;
+        }
+        for i in 0..replicas_len {
+            if processed_replicas >= replicas_len {
+                break 'main_loop;
+            }
+            processed_replicas += 1;
+            let mut stream = replicas
+                .unwrap()
+                .get(i)
+                .context(fdbg!("[{}] Unable to get replica from cache", i))?;
+            stream
+                .write_all(&get_ack_command.as_bytes())
+                .context(fdbg!("[{}] Unable to write to stream for get ack", i))?;
+            let (cmd, flag, offset) = run_get_ack(stream)?;
+            debug!(
+                "[{}] Received ack from replica: {} {} {}",
+                i, cmd, flag, offset
+            );
+            ack_received += 1;
+            if ack_received >= nacks_wanted {
+                break 'main_loop;
+            }
+            if start_time.elapsed().as_millis() > timeout_ms as u128 {
+                break 'main_loop;
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+    return Ok(DataType::Integer(ack_received as u64));
 }
 
 // pub fn process_wait_cmd(
