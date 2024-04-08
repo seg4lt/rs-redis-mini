73fa8f591b34b6694f9cff77d576c03ad6c1abe2
seg4lt <12027886+seg4lt@users.noreply.github.com>
2024-04-07 22:12:15 -0500

    REPLCONF

--- src/conn_with_master/mod.rs
+++ src/conn_with_master/mod.rs
@@ -21,15 +21,52 @@ pub(crate) async fn prepare_conn_with_master() -> anyhow::Result<()> {
     tokio::spawn(async move {
         let (reader, mut writer) = stream.split();
         let mut reader = BufReader::new(reader);
+        // PING
         let ping = RESPType::Array(vec![RESPType::BulkString("PING".to_string())]);
         writer
             .write_all(&ping.as_bytes())
             .await
             .expect("Should be able to write PING");
         writer.flush().await.expect("Should be able to flush PING");
-        let response = parse_request(&mut reader)
+        let _response = parse_request(&mut reader)
             .await
             .expect("Should be able to parse PONG");
+        // REPL CONF
+        let port = AppConfig::get_port();
+        let repl_conf_listening_port = RESPType::Array(vec![
+            RESPType::BulkString("REPLCONF".to_string()),
+            RESPType::BulkString("listening-port".to_string()),
+            RESPType::BulkString(format!("{port}")),
+        ]);
+        writer
+            .write_all(&repl_conf_listening_port.as_bytes())
+            .await
+            .expect("Should be able to write replconf listening-port");
+        writer
+            .flush()
+            .await
+            .expect("Should be able to flush replconf listening-port");
+        // let _response = parse_request(&mut reader)
+        //     .await
+        //     .expect("Should be able to parse OK");
+
+        // REPL capa psync2
+        let repl_conf_capa_psync2 = RESPType::Array(vec![
+            RESPType::BulkString("REPLCONF".to_string()),
+            RESPType::BulkString("capa".to_string()),
+            RESPType::BulkString("psync2".to_string()),
+        ]);
+        writer
+            .write_all(&repl_conf_capa_psync2.as_bytes())
+            .await
+            .expect("Should be able to write replconf capa psync2");
+        writer
+            .flush()
+            .await
+            .expect("Should be able to flush replconf capa psync2");
+        // let _response = parse_request(&mut reader)
+        //     .await
+        //     .expect("Should be able to parse OK");
     });
     Ok(())
 }
